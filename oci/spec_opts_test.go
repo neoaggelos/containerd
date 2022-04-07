e61d8ce3d4d2bc5cb9e15f2c520590b226c4d7d2
Angelos Kolaitis <neoaggelos@gmail.com>
2022-04-07 13:43:00 +0300

    Backport GHSA-c9cp-9c75-9v8c to 1.4
    [release/1.4] Fix the Inheritable capability defaults.

--- oci/spec_opts_test.go
+++ oci/spec_opts_test.go
@@ -585,7 +585,6 @@ func TestDropCaps(t *testing.T) {
 		s.Process.Capabilities.Bounding,
 		s.Process.Capabilities.Effective,
 		s.Process.Capabilities.Permitted,
-		s.Process.Capabilities.Inheritable,
 	} {
 		if capsContain(cl, "CAP_CHOWN") {
 			t.Errorf("cap list %d contains dropped cap", i)
